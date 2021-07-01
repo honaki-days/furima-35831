a530ba9677f1d13b8d7c0945686d054068a62650
honaki-days <ice.make.sky.sakura@icloud.com>
2021-07-01 19:13:11 +0900

    set_user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update]
+  before_action :item_find, only: [:show, :create, :edit, :update]
+  before_action :set_user, only: [:edit, :update]
   
   def index
     @items = Item.order("created_at DESC")
@@ -10,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -19,19 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user == @item.user
   end
 
   def destroy
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -42,5 +39,13 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :description, :detail_category_id, :detail_condition_id, :user_area_id, :delivery_charge_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
+  def set_user
+    redirect_to root_path unless current_user == @item.user
   end
 end
