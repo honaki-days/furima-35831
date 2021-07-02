08be6f1f4337957698582bbb585746a3d9fd6bbb
honaki-days <ice.make.sky.sakura@icloud.com>
2021-07-02 19:02:46 +0900

    before_action :set_user, only: [:edit, :update, :destroy]

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :destroy]
   
   def index
     @items = Item.order("created_at DESC")
