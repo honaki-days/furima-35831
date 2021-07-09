b7ca75d6fdf93549b40a4777d95c4e579927f2a9
honaki-days <ice.make.sky.sakura@icloud.com>
2021-07-09 12:55:24 +0900

    id

--- app/controllers/purchase_logs_controller.rb
+++ app/controllers/purchase_logs_controller.rb
@@ -23,7 +23,7 @@ class PurchaseLogsController < ApplicationController
   private
 
   def purchase_log_params
-    params.require(:purchase_log_address).permit(:receiver_postal_code, :user_area_id, :receiver_cities, :receiver_address, :receiver_building_name, :receiver_phone_number, :purchases_log).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_log_address).permit(:receiver_postal_code, :user_area_id, :receiver_cities, :receiver_address, :receiver_building_name, :receiver_phone_number, :purchases_log_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
